15d20d187dea6c9661e33f714a1c1e962da29289
gwilli91 <71255658+gwilli91@users.noreply.github.com>
2025-04-23 11:21:29 +0200

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -37,4 +37,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+  host: true,
+  port: 5173,
+  strictPort: true,
+  allowedHosts: 'all',
+},
 })
